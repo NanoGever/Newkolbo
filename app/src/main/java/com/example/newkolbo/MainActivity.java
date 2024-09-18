13d9692f87a4874407b189e6e7138d1c9478b773
NoamsProject <76526786+NoamsProject@users.noreply.github.com>
2024-09-19 00:31:08 +0300

    NAah brooooooo naahhh
    frrrrrrrrrrrrr
    12:30 AMMMM
    9/19

--- app/src/main/java/com/example/newkolbo/MainActivity.java
+++ app/src/main/java/com/example/newkolbo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.newkolbo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,19 +12,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button order;                   // Order button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.order1);
+        setContentView(R.layout.activity_main);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+                ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                    return insets;
 
+                });
+        order=findViewById(R.id.button_main_order);                               //order button takes to order page 2
+        order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainOrder.class);
+                startActivity(intent);
+            }
+
+            ;
         });
-    }
-}
+    };
+};
