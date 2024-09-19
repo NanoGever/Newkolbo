9e6a454287953d48b3eb2510ff9763e8f633f20d
NoamsProject <76526786+NoamsProject@users.noreply.github.com>
2024-09-19 22:22:50 +0300

    fuickkkk
    9/19
    10:22 PMMMMMMM KILLLLLLL ME

--- app/src/main/java/com/example/newkolbo/OrderCredit.java
+++ app/src/main/java/com/example/newkolbo/OrderCredit.java
@@ -1,8 +1,6 @@
 package com.example.newkolbo;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -11,26 +9,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainOrder extends AppCompatActivity {
-
-    private Button continue1;
+public class OrderCredit extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.mainorder);
+        setContentView(R.layout.activity_order_credit);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        continue1 = findViewById(R.id.continueid1);                               //continue button takes to order page Ordercredit
-        continue1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainOrder.this, OrderCredit.class);
-                startActivity(intent);
 
-            }
-        });}}
+        });
+    }
+}
